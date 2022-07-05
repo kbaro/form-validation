e5c79bc60842c98f7db5efc152c04b689c9ba911
Kristijan Baro <baro.kristijan@gmail.com>
2022-07-05 20:17:55 +0200

    JS start

---
+++ script.js
@@ -0,0 +1,12 @@
+let inputs = document.querySelectorAll('input');
+let errors = {
+    "ime_prezime": [],
+    "korisnicko_ime": [],
+    "email": [],
+    "lozinka": [],
+    "ponovi_lozinku": []
+};
+
+inputs.forEach(element => {
+    
+});
